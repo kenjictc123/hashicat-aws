00b3f84af4a9469ef2fc38233a3d68ac3b0d722d
kenjictc123 <97501648+kenjictc123@users.noreply.github.com>
2022-07-26 15:59:51 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,15 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_ACCESS_KEY_ID" {}
 
-terraform {
-  # 使用するAWSプロバイダーのバージョン指定（結構更新が速い）
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>3.42"
-    }
-  }
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = "ap-northeast-1"
 }
 
-# 明示的にAWSプロバイダを定義（暗黙的に理解してくれるけど）
-provider "aws" {
-  profile = "default"
-  region  = "ap-northeast-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-
-
-
-  # 作成する全リソースに自動的に付与するタグ設定
-  default_tags {
-    tags = {
-      env = "terraform_test"
-    }
+resource "aws_vpc" "test-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "test-vpc"
   }
 }
