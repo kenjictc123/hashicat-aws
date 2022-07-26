c07740ac8a80dcd9feeded1c762c8e8417db1b03
kenjictc123 <97501648+kenjictc123@users.noreply.github.com>
2022-07-26 18:06:48 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
-variable "aws_region" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_REGION" {}
 variable "aws_vpc_cidr" {}
 variable "prefix" {}
 
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
-  region = var.aws_region
+  region = var.AWS_REGION
 }
 
 resource "aws_vpc" "test-vpc" {
@@ -15,37 +15,4 @@ resource "aws_vpc" "test-vpc" {
   tags = {
     Name = "test-vpc"
   }
-}
-
-# -----------------------------------
-# S3の作成
-# -----------------------------------
-### バケット作成 ###
-resource "aws_s3_bucket" "terraform_test" {
-  # S3のバケット名
-  bucket = "test-bucket-20220726-hisa" ###要変更###
-  # アクセス管理
-  acl = "private"
-  # バージョニングの有効化
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "terraform_test" {
-  # 対象のバケット
-  bucket = aws_s3_bucket.terraform_test.id
-  # パブリックのアクセスをブロック
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
