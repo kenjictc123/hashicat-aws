c1ea61294b5d4003e3144875de5624be13bb715e
kenjictc123 <97501648+kenjictc123@users.noreply.github.com>
2022-07-26 14:48:27 +0900

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hisanaga-training"
+    organization = "hisaken-test"
     workspaces {
       name = "hashicat-aws"
     }
