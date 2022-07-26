d2230c46f5395aa71333b34265531e2650c906c1
kenjictc123 <97501648+kenjictc123@users.noreply.github.com>
2022-07-26 18:11:48 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-variable "AWS_REGION" {}
-variable "AWS_VPC_CIDR" {}
-variable "prefix" {}
-
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
